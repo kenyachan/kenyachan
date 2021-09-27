9e41bb6b6e15bc83a89e8aee9fb42dec4aa06c91
Kenya Chan <kenya.chan@gmail.com>
2021-09-27 21:10:58 +1000

    Move file to js folder

--- script.js
+++
@@ -1,9 +0,0 @@
-function doSomething() {
-    var pageText = document.getElementById("text").innerHTML;
-
-    if (pageText == "") {
-        document.getElementById("text").innerHTML = "Button clicked!";
-    } else {
-        document.getElementById("text").innerHTML = "";
-    }
-}
