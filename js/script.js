46c299044ca9e69359eedbc6e25ab7ddc80c901d
Kenya Chan <kenya.chan@gmail.com>
2021-09-28 21:19:01 +1000

    Add function to get profile details

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function doSomething() {
-    var pageText = document.getElementById("text").innerHTML;
-
-    if (pageText == "") {
-        document.getElementById("text").innerHTML = "Button clicked!";
-    } else {
-        document.getElementById("text").innerHTML = "";
-    }
+function getDetails() {
+    fetch(`https://api.github.com/users/kenyachan`)
+    .then(response=>response.json().then(data=> {
+        //console.log(data);
+        document.getElementById('name-text').innerHTML=data.name;
+        document.getElementById('description-text').innerHTML=data.bio;
+        document.getElementById('github-link').setAttribute('href', data.html_url)
+    }))
 }
